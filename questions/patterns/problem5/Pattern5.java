b2f2a880c9948563526dd5e35474ff5ab3a8e0bc
Anish Anantharaman <anish.pgt@gmail.com>
2026-01-11 23:26:09 +0530

    feat: add diamond pattern problem

---
+++ questions/patterns/problem5/Pattern5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Pattern5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int sp = n/2;
+        int st = 1;
+        for(int i=1; i<=n; ++i) {
+            for(int j = 1; j<= sp; ++j) {
+                System.out.print("\t");
+            }
+
+            for(int j = 1; j <= st; ++j) {
+                System.out.print("*\t");
+            }
+
+            System.out.println();
+            if(i <= (n/2)) {
+                --sp;
+                st+=2;
+            } else {
+                ++sp;
+                st-=2;
+            }
+        }
+    }
+}
