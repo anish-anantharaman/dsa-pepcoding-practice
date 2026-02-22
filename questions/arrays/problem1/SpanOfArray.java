a73d3510dd5a4eaa991ba179699897d97bb13f11
Anish Anantharaman <anish.pgt@gmail.com>
2026-02-22 19:26:27 +0530

    feat: add span of an array problem

---
+++ questions/arrays/problem1/SpanOfArray.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class SpanOfArray {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for(int i = 1; i < n; ++i) {
+
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+
+            if(arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        int span = max - min;
+        System.out.println(span);
+    }
+}
